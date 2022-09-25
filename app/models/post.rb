7ec3dec6d7f9590807fb729dee3431e838bfb0c4
wandera7 <julius.wandera@student.moringaschool.com>
2022-09-25 09:58:00 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category,  inclusion: { in: ['Fiction', 'Non-Fiction']}
+  validate :title_is_clickbait
+
+  def title_is_clickbait
+    unless title =="You Won't Believe These True Facts"
+      errors.add(:title, "Title should be You Won't Believe These True Facts ")
+    end
+  end
 end
